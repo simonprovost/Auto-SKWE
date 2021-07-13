1961de9ee0e4cce89d64b7e5f3020cace8da3125
simonProvost <simon1.provost@epitech.eu>
2021-07-13 19:57:57 +0100

    feat(analysis): add main.py for the obesity dataset

---
+++ main_obesity_dataset.py
@@ -0,0 +1,43 @@
+import sys
+
+from src.preProcess import *
+from src.processing import Processing
+from src.utils import getDataFromCsv, readParamsFile
+
+
+def main(argv):
+    ### DATA PRE PROCESSING
+    dataset = getDataFromCsv('./datasets/obesity-data/ObesityDataSet_raw_and_data_sinthetic.csv')
+    input = dataset[1]
+    classData = dataset[2]
+
+    data = PreProcess(input, classData)
+    data.genericFeatureScaling(inputData=False, columnName="NObeyesdad",
+                               initialArray=['Insufficient_Weight',
+                                             'Normal_Weight',
+                                             'Overweight_Level_I',
+                                             'Overweight_Level_II',
+                                             'Obesity_Type_I',
+                                             'Obesity_Type_II',
+                                             'Obesity_Type_III'],
+                               outputArray=[0, 1, 2, 3, 4, 5, 6])
+    data.redefineColumnsType()
+
+    X = data.input
+    y = np.array(data.output['NObeyesdad']).tolist()
+
+    ### AUTO ML PROCESSING
+
+    params = readParamsFile(argv[1])
+
+    autoML = Processing(inputData=X, classLabelData=y, datasetName="ObesityDataSet_raw_and_data_synthetic", **params)
+    autoML.setup()
+    autoML.fit_predict()
+    models = autoML.getBestModels(numberOfModelToGet=0, display=False)
+    autoML.showLatexTable(models[0])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        raise ValueError("Need path to the params to use!")
+    main(sys.argv)
