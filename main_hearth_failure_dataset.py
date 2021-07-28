1d956ffc7d26a77475cf3cda880b5f24119fbac3
simonProvost <simon1.provost@epitech.eu>
2021-07-28 12:56:08 +0100

    feat(datasets): add new datasets and their analysis

---
+++ main_hearth_failure_dataset.py
@@ -0,0 +1,33 @@
+import sys
+
+from src.preProcess import *
+from src.processing import Processing
+from src.utils import getDataFromCsv, readParamsFile
+
+
+def main(argv):
+    ### DATA PRE PROCESSING
+    dataset = getDataFromCsv('./datasets/heart-failure/Heart-failure-clinical-records_dataset.csv')
+    input = dataset[1]
+    classData = dataset[2]
+
+    data = PreProcess(input, classData, datasetName="Heart-failure-clinical-records_dataset")
+    data.redefineColumnsType()
+
+    X = data.input
+    y = data.output
+
+    ### AUTO ML PROCESSING
+    params = readParamsFile(argv[1])
+
+    autoML = Processing(inputData=X, classLabelData=y, datasetName=data.datasetName, **params)
+    autoML.setup()
+    autoML.fit_predict()
+    models = autoML.getBestModels(numberOfModelToGet=0, display=False)
+    autoML.showLatexTable(models[0])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        raise ValueError("Need path to the params to use!")
+    main(sys.argv)
